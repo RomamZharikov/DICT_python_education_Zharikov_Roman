03e98319afcc35080c09fbf3639948b7ec7ad24d
Roman Zharikov <r.o.zharikov@student.khai.edu>
2021-10-14 19:50:37 +0300

    Hangman 4-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -7,8 +7,21 @@ hidden_word = ["python", "java", "javascript", "php"]
 selected_word = hidden_word[index_word]
 word = str()
 
+letters = list(selected_word)
+hidden = letters[3:]
+prompt = letters[:3]
+number = len(hidden)
+hidden_letters = []
+car = ["-"]
+
+for x in range(number):
+    hidden_letters = hidden_letters + car
+
+hint = prompt + hidden_letters
+ready_hint = "".join(hint)
+
 while word != selected_word:
-    word = input("")
+    word = input(ready_hint + ": ")
     if word == selected_word:
         print("You survived!")
     else:
