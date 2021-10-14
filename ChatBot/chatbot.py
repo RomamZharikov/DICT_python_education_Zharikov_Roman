54ddc1bd1d6446a9be451140a3e077e2e6143790
Roman Zharikov <r.o.zharikov@student.khai.edu>
2021-10-14 19:26:38 +0300

    ChatBot 2-nd stage

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,2 +1,5 @@
 print("""Hello! My name is Andrew.
-I was created in 2021.""")
+I was created in 2021.
+Please, remind me your name.""")
+name = input(" ")
+print(f"What a great name you have, {name}!")
