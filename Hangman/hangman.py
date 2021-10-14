95c841194a77a4ba8281647eb4eb442c6358c4f7
Roman Zharikov <r.o.zharikov@student.khai.edu>
2021-10-14 19:50:08 +0300

    Hangman 3-rd stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,11 +1,15 @@
+import random
 print("""Hello! Welcome to the game Hangman!
 Try to guess the word.""")
-hidden_word = str("python")
+
+index_word = random.randint(0, 3)
+hidden_word = ["python", "java", "javascript", "php"]
+selected_word = hidden_word[index_word]
 word = str()
 
-while word != hidden_word:
-    word = str(input("Guess the word:"))
-    if word == hidden_word:
+while word != selected_word:
+    word = input("")
+    if word == selected_word:
         print("You survived!")
     else:
         print("You lost!")
