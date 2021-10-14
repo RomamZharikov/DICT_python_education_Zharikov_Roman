0f787e332875885404a6b25fda1191fd9f22792c
Roman Zharikov <r.o.zharikov@student.khai.edu>
2021-10-14 19:52:11 +0300

    Hangman 7-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -21,16 +21,31 @@ def game():
         print("")
         letter = input("Input a letter:")
         if letter in try_word:
-            print("No improvements")
-            hp -= 1
-            if hp == 0:
-                print("You lost!")
-                break
+            print("You've already guessed this letter.")
+            print("")
+            lines()
+            continue
+        if letter in try_wrong:
+            print("You've already guessed this letter.")
+            print("")
+            lines()
+            continue
         for i in random_word:
             if letter == i:
                 try_word.append(letter)
+        if letter.capitalize() == letter and len(letter) == 1:
+            print("Please enter a lowercase English letter.")
+            print("")
+            lines()
+            continue
+        if len(letter) == 0 or len(letter) >= 2:
+            print("You should input a single letter.")
+            print("")
+            lines()
+            continue
         if letter not in random_word:
-            print("That letter doesn't appear in the word")
+            try_wrong.append(letter)
+            print("That letter doesn't appear in the word.")
             hp -= 1
             if hp == 0:
                 print("You lost!")
@@ -39,7 +54,7 @@ def game():
         lines()
         if random_word_set == set(try_word):
             print('')
-            print("You guessed the word!")
+            print("You guessed the word " + random_word + "!")
             print("You survived!")
             print('')
             break
@@ -49,4 +64,3 @@ print('HANGMAN')
 while True:
     print(len(random_word) * '-', end='')
     game()
-    break
