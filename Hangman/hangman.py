99ab194099b4d13cb400ed773872a0f36b339730
Roman Zharikov <r.o.zharikov@student.khai.edu>
2021-10-14 19:49:35 +0300

    Hangman 2-nd stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,2 +1,12 @@
-print("""HANGMAN
-The game will be available soon.""")
+print("""Hello! Welcome to the game Hangman!
+Try to guess the word.""")
+hidden_word = str("python")
+word = str()
+
+while word != hidden_word:
+    word = str(input("Guess the word:"))
+    if word == hidden_word:
+        print("You survived!")
+    else:
+        print("You lost!")
+        break
