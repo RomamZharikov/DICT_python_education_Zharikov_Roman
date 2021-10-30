2ba6e76e21d198fe1527d4ae0bbe66d5a869a0d9
Roman Zharikov <r.o.zharikov@student.khai.edu>
2021-10-30 20:55:18 +0300

    TicTacToe 2-nd stage

--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -1,3 +1,19 @@
-print("""X O X
-O X O
-X X O""")
+print(f"""---------
+| _ _ _ |
+| _ _ _ |
+| _ _ _ |
+---------""")
+
+
+while True:
+    entry = input("Please, enter a string consisting of 9 characters 'X', 'O' or '_': ")
+    data = list(entry)
+    if len(entry) == 9:
+        print(f"""---------
+| {data[0]} {data[1]} {data[2]} |
+| {data[3]} {data[4]} {data[5]} |
+| {data[6]} {data[7]} {data[8]} |
+---------""")
+        break
+    else:
+        print("try again")
