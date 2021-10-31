4271a985b6e860bc9741ec00a27f8942aa4beb6e
Roman Zharikov <r.o.zharikov@student.khai.edu>
2021-10-31 16:17:09 +0300

    TicTacToe 4-th stage

--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -1,51 +1,132 @@
-print(f"""{"-" * 9}
-| _ _ _ |
-| _ _ _ |
-| _ _ _ |
-{"-" * 9}""")
+list_board = list(input("Enter cells: "))
+board_x = list_board.count("X")
+board_o = list_board.count("O")
+board__ = list_board.count("_")
+winners = 0
 
 
-def checkout():
-    amount_x = data.count("X")
-    amount_o = data.count("O")
-    amount__ = data.count("_")
-    if amount_x == amount_o or amount_x == amount_o + 1 or amount_x == amount_o - 1:
-        if amount__ == 0:
-            if data[0] == data[3] == data[6] != "_":
-                print(f"{data[0]} win!")
-            elif data[1] == data[4] == data[7] != "_":
-                print(f"{data[1]} win!")
-            elif data[2] == data[5] == data[8] != "_":
-                print(f"{data[2]} win!")
-            elif data[0] == data[4] == data[8] != "_":
-                print(f"{data[0]} win!")
-            elif data[2] == data[4] == data[6] != "_":
-                print(f"{data[2]} win!")
-            elif data[0] == data[1] == data[2] != "_":
-                print(f"{data[0]} win!")
-            elif data[3] == data[4] == data[5] != "_":
-                print(f"{data[3]} win!")
-            elif data[6] == data[7] == data[8] != "_":
-                print(f"{data[6]} win!")
-            else:
-                print("Draw")
-        else:
-            print("Game not finish")
-    else:
-        print("Impossible")
+def board():
+    print("-" * 9)
+    print(f"""|{list_board[0]} {list_board[1]} {list_board[2]}|
+|{list_board[3]} {list_board[4]} {list_board[5]}|
+|{list_board[6]} {list_board[7]} {list_board[8]}|""")
+    print("-" * 9)
 
 
+def rs_lt():
+    global winners
+    if list_board[0] == list_board[1] == list_board[2] != "_":
+        print(list_board[0] + " wins")
+        winners += 1
+    elif list_board[3] == list_board[4] == list_board[5] != "_":
+        print(list_board[3] + " wins")
+        winners += 1
+    elif list_board[6] == list_board[7] == list_board[8] != "_":
+        print(list_board[6] + " wins")
+        winners += 1
+    elif list_board[2] == list_board[5] == list_board[8] != "_":
+        print(list_board[2] + " wins")
+        winners += 1
+    elif list_board[0] == list_board[3] == list_board[6] != "_":
+        print(list_board[0] + " wins")
+        winners += 1
+    elif list_board[0] == list_board[3] == list_board[6] != "_":
+        print(list_board[0] + " wins")
+        winners += 1
+    elif list_board[1] == list_board[4] == list_board[7] != "_":
+        print(list_board[1] + " wins")
+        winners += 1
+    elif list_board[2] == list_board[5] == list_board[8] != "_":
+        print(list_board[2] + " wins")
+        winners += 1
+    elif board__ < 1:
+        winners += 1
+        print("Draw")
+
+
+board()
+player = "X"
 while True:
-    entry = input("Please, enter a string consisting of 9 characters 'X', 'O' or '_': ")
-    data = list(entry)
-    if len(entry) == 9:
-        print(f"""---------
-| {data[0]} {data[1]} {data[2]} |
-| {data[3]} {data[4]} {data[5]} |
-| {data[6]} {data[7]} {data[8]} |
----------""")
-        checkout()
-        break
+    take = input("Enter the coordinates: ")
+    coord = list(take)
+    x, y = str(coord[2]), str(coord[0])
+    if x.isalpha() or y.isalpha() or x == " " or y == " ":
+        print("You should enter numbers!")
+        continue
+    elif y == "1":
+        if x == "1":
+            if str(list_board[0]) != "X" and list_board[0] != "O":
+                list_board[0] = player
+            else:
+                print("This cell is occupied! Choose another one!")
+                continue
+        elif x == "2":
+            if list_board[1] != "X" and list_board[1] != "O":
+                list_board[1] = player
+            else:
+                print("This cell is occupied! Choose another one!")
+                continue
+        elif x == "3":
+            if list_board[2] != "X" and list_board[2] != "O":
+                list_board[2] = player
+            else:
+                print("This cell is occupied! Choose another one!")
+                continue
+        else:
+            print("Coordinates should be from 1 to 3!")
+            continue
+    elif y == "2":
+        if x == "1":
+            if list_board[3] != "X" and list_board[3] != "O":
+                list_board[3] = player
+            else:
+                print("This cell is occupied! Choose another one!")
+                continue
+        elif x == "2":
+            if list_board[4] != "X" and list_board[4] != "O":
+                list_board[4] = player
+            else:
+                print("This cell is occupied! Choose another one!")
+                continue
+        elif x == "3":
+            if list_board[5] != "X" and list_board[5] != "O":
+                list_board[5] = player
+            else:
+                print("This cell is occupied! Choose another one!")
+                continue
+        else:
+            print("Coordinates should be from 1 to 3!")
+            continue
+    elif y == "3":
+        if x == "1":
+            if list_board[6] != "X" and list_board[6] != "O":
+                list_board[6] = player
+            else:
+                print("This cell is occupied! Choose another one!")
+                continue
+        elif x == "2":
+            if list_board[7] != "X" and list_board[7] != "O":
+                list_board[7] = player
+            else:
+                print("This cell is occupied! Choose another one!")
+                continue
+        elif x == "3":
+            if list_board[8] != "X" and list_board[8] != "O":
+                list_board[8] = player
+            else:
+                print("This cell is occupied! Choose another one!")
+                continue
+        else:
+            print("Coordinates should be from 1 to 3!")
+            continue
     else:
-        print("try again")
-
+        print("Coordinates should be from 1 to 3!")
+        continue
+    board()
+    rs_lt()
+    if winners > 0:
+        break
+    if player == "X":
+        player = "O"
+    else:
+        player = "X"
