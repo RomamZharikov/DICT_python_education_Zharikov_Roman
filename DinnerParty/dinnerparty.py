6446d4301cb910eb5bf4d2b5baf55e493dbc506d
Roman Zharikov <r.o.zharikov@student.khai.edu>
2021-11-12 23:20:33 +0200

    DinnerParty 2-nd stage

--- DinnerParty/dinnerparty.py
+++ DinnerParty/dinnerparty.py
@@ -4,5 +4,24 @@ if friends > 0:
     name = [input() for i in range(friends)]
     new_dict = {name[z - 1]: 0 for z in range(1, friends + 1)}
     print(new_dict)
+    amount = int(input("Enter the total amount: "))
+    if int(amount / friends) != (amount / friends):
+        list_amount = [round((amount / friends), 2)] * friends
+    else:
+        list_amount = [round(amount / friends)] * friends
+    new_dict_keys = list(new_dict.keys())
+    new_dict_keys.sort()
+    for i in range(len(new_dict_keys)):
+        new_dict[new_dict_keys[i]] = list_amount[i]
+    print(new_dict)
+else:
+    print("No one is joining for the party.")
+
+friends = int(input("Enter the number of friends joining (including you):"))
+if friends > 0:
+    print("Enter the name of every friend (including you), each on a new line:")
+    name = [input() for i in range(friends)]
+    new_dict = {name[z - 1]: 0 for z in range(1, friends + 1)}
+    print(new_dict)
 else:
     print("No one is joining for the party.")
