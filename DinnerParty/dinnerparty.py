8998e75e44f55f58264fc3bd6cf7c45c30c78ac2
Roman Zharikov <r.o.zharikov@student.khai.edu>
2021-11-13 12:14:38 +0200

    DinnerParty 3-rd stage

--- DinnerParty/dinnerparty.py
+++ DinnerParty/dinnerparty.py
@@ -1,9 +1,10 @@
+import random
+
 friends = int(input("Enter the number of friends joining (including you):"))
 if friends > 0:
     print("Enter the name of every friend (including you), each on a new line:")
     name = [input() for i in range(friends)]
     new_dict = {name[z - 1]: 0 for z in range(1, friends + 1)}
-    print(new_dict)
     amount = int(input("Enter the total amount: "))
     if int(amount / friends) != (amount / friends):
         list_amount = [round((amount / friends), 2)] * friends
@@ -11,17 +12,16 @@ if friends > 0:
         list_amount = [round(amount / friends)] * friends
     new_dict_keys = list(new_dict.keys())
     new_dict_keys.sort()
-    for i in range(len(new_dict_keys)):
-        new_dict[new_dict_keys[i]] = list_amount[i]
-    print(new_dict)
-else:
-    print("No one is joining for the party.")
-
-friends = int(input("Enter the number of friends joining (including you):"))
-if friends > 0:
-    print("Enter the name of every friend (including you), each on a new line:")
-    name = [input() for i in range(friends)]
-    new_dict = {name[z - 1]: 0 for z in range(1, friends + 1)}
+    choice = input("Do you want to use the 'Who is lucky?' feature? Write Yes/No: ")
+    if choice == "Yes":
+        lucky_friend_index = random.choice(range(friends))
+        for i in range(len(new_dict_keys)):
+            new_dict[new_dict_keys[i]] = list_amount[i]
+        print(f"{new_dict_keys[lucky_friend_index]} is the lucky one!")
+    elif choice == "No":
+        for i in range(len(new_dict_keys)):
+            new_dict[new_dict_keys[i]] = list_amount[i]
+        print("No one is going to be lucky.")
     print(new_dict)
 else:
     print("No one is joining for the party.")
