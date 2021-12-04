80167ac9e8b595aef4a633e0912b7898ecb2c8c4
Roman Zharikov <r.o.zharikov@student.khai.edu>
2021-12-04 18:53:36 +0200

    Rock-Paper-Scissors 2-nd stage

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -1,9 +1,18 @@
-choice = {"rock": "paper", "paper": "scissors", "scissors": "rock"}
+from random import choice
+
+losing = {"rock": "paper", "paper": "scissors", "scissors": "rock"}
+
 if __name__ == "__main__":
     while True:
         input_player = input()
-        if input_player in choice.keys():
-            print(f"Sorry, but the computer chose {choice.get(input_player)}.")
+        if input_player in losing.keys():
+            choice_pc = choice(list(losing.keys()))
+            if choice_pc == losing.get(input_player):
+                print(f"Sorry, but the computer chose {choice_pc}.")
+            elif choice_pc == input_player:
+                print(f"There is a draw ({choice_pc}).")
+            elif losing.get(choice_pc) == input_player:
+                print(f"Well done. The computer chose {choice_pc} and failed.")
             break
         else:
-            print("Error")
+            print("Invalid input. Please, try again.")
