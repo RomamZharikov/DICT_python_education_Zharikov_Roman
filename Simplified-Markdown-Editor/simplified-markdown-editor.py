b4b111d857915107e0a1740d586ad2f4d9f1601d
Roman Zharikov <r.o.zharikov@student.khai.edu>
2022-02-14 14:47:46 +0200

    Simplified Markdown Editor 2-nd stage

--- Simplified-Markdown-Editor/simplified-markdown-editor.py
+++ Simplified-Markdown-Editor/simplified-markdown-editor.py
@@ -2,14 +2,41 @@ class SimplifiedMarkdownEditor(object):
     def __init__(self):
         self.command = ["plain", "bold", "italic", "header", "link", "inline-code",
                         "ordered-list", "unordered-list", "new-line"]
+        self.edit = []
 
     def help(self):
         print("Available formatters:")
-        for i in self.command:
-            print("*" + i)
+        for j in self.command:
+            print("*" + j)
 
     def done(self):
         pass
+
+    def header(self):
+        while True:
+            while True:
+                try:
+                    num = int(input("Level: "))
+                    break
+                except ValueError:
+                    print("Please enter a number!")
+            if num <= 6:
+                text = input("Text: ")
+                self.edit.append(num * "#" + " " + text)
+                break
+            else:
+                print("The level should be within the range of 1 to 6.")
+
+    def plain(self):
+        self.edit.append(input("Text: "))
+
+    def new_line(self):
+        self.edit.append("")
+
+    def link(self):
+        label = input("Label: ")
+        url = input("URL: ")
+        self.edit.append(f"[{label}]({url})")
 
 
 if __name__ == "__main__":
@@ -17,12 +44,28 @@ if __name__ == "__main__":
     print("=" * 70)
     print("Welcome to the Simplified Markdown Editor!")
     while True:
-        print("=" * 70)
-        formatter = input("Choose a formatter: \n").lower().strip()
+        if len(x.edit) == 0:
+            print("=" * 70)
+            formatter = input("Choose a formatter: \n").lower().strip()
+        else:
+            print("=" * 70 + "\nYour text is:")
+            for i in x.edit:
+                print(i)
+            print("=" * 70)
+            formatter = input("Choose a formatter: \n").lower().strip()
         if formatter == "!help":
             x.help()
         elif formatter == "!done":
             x.done()
             break
         else:
-            print("Unknown formatting type or command")
+            if formatter == "header" or formatter == "h":
+                x.header()
+            elif formatter == "plain" or formatter == "p":
+                x.plain()
+            elif formatter == "new-line" or formatter == "nl":
+                x.new_line()
+            elif formatter == "link" or formatter == "l":
+                x.link()
+            else:
+                print("Unknown formatting type or command")
