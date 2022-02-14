92ba011b64477f2312a3848f59ec981b4df2d2f6
Roman Zharikov <r.o.zharikov@student.khai.edu>
2022-02-14 15:21:15 +0200

    Simplified Markdown Editor 3-rd stage

--- Simplified-Markdown-Editor/simplified-markdown-editor.py
+++ Simplified-Markdown-Editor/simplified-markdown-editor.py
@@ -38,34 +38,64 @@ class SimplifiedMarkdownEditor(object):
         url = input("URL: ")
         self.edit.append(f"[{label}]({url})")
 
+    @staticmethod
+    def input_list():
+        while True:
+            try:
+                num = int(input("Number of rows: "))
+                if num > 0:
+                    break
+                else:
+                    print("The number of rows should be greater than zero!")
+            except ValueError:
+                print("Please enter a number!")
+        return [input(f"Row #{k}: ") for k in range(1, num + 1)]
+
+    def ordered_list(self):
+        append_list = self.input_list()
+        for k in range(len(append_list)):
+            self.edit.append(f"{k + 1}. {append_list[k]}")
+
+    def unordered_list(self):
+        append_list = self.input_list()
+        for k in range(len(append_list)):
+            self.edit.append(f"* {append_list[k]}")
+
+    def main(self):
+        print("=" * 70)
+        print("Welcome to the Simplified Markdown Editor!")
+        while True:
+            if len(self.edit) == 0:
+                print("=" * 70)
+                formatter = input("Choose a formatter: \n").lower().strip()
+            else:
+                print("=" * 70 + "\nYour text is:")
+                for i in self.edit:
+                    print(i)
+                print("=" * 70)
+                formatter = input("Choose a formatter: \n").lower().strip()
+            if formatter == "!help":
+                self.help()
+            elif formatter == "!done":
+                self.done()
+                break
+            else:
+                if formatter == "header" or formatter == "h":
+                    self.header()
+                elif formatter == "plain" or formatter == "p":
+                    self.plain()
+                elif formatter == "new-line" or formatter == "nl":
+                    self.new_line()
+                elif formatter == "link" or formatter == "l":
+                    self.link()
+                elif formatter == "ordered-list" or formatter == "ol":
+                    self.ordered_list()
+                elif formatter == "unordered-list" or formatter == "ul":
+                    self.unordered_list()
+                else:
+                    print("Unknown formatting type or command")
+
 
 if __name__ == "__main__":
     x = SimplifiedMarkdownEditor()
-    print("=" * 70)
-    print("Welcome to the Simplified Markdown Editor!")
-    while True:
-        if len(x.edit) == 0:
-            print("=" * 70)
-            formatter = input("Choose a formatter: \n").lower().strip()
-        else:
-            print("=" * 70 + "\nYour text is:")
-            for i in x.edit:
-                print(i)
-            print("=" * 70)
-            formatter = input("Choose a formatter: \n").lower().strip()
-        if formatter == "!help":
-            x.help()
-        elif formatter == "!done":
-            x.done()
-            break
-        else:
-            if formatter == "header" or formatter == "h":
-                x.header()
-            elif formatter == "plain" or formatter == "p":
-                x.plain()
-            elif formatter == "new-line" or formatter == "nl":
-                x.new_line()
-            elif formatter == "link" or formatter == "l":
-                x.link()
-            else:
-                print("Unknown formatting type or command")
+    x.main()
