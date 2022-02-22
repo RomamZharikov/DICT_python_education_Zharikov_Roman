1ae9dced96ce0ca54fe0847a6671fd785d0894a3
Roman Zharikov <r.o.zharikov@student.khai.edu>
2022-02-22 11:02:35 +0200

    Currency Exchange 4-th stage

--- Currency_Exchange/currency_exchange.py
+++ Currency_Exchange/currency_exchange.py
@@ -1,34 +1,59 @@
-from parse import Parse
+from parse import Parse, code_current
 from json import JSONDecodeError
 
 
 class CurrencyExchange:
-    def __init__(self, current_code):
-        self.code = current_code
-        self.my_coin = 0
+    def __init__(self, current_code_in, coin, current_code_out):
+        self.code_in = current_code_in
+        self.code_out = current_code_out
+        self.coin = coin
+        self.used_coin = 0
+        self.rate = Parse(self.code_in, self.code_out.lower())
 
     def conversion(self):
         while True:
             try:
-                rate = Parse(self.code).request()
+                self.rate.currency_in = self.code_in
+                self.rate.currency_out = self.code_out.lower()
                 break
             except JSONDecodeError:
                 print("Please, enter a valid currency code")
-                self.code = input("Please, enter currency code: ")
-        for i in rate:
-            print(f"The {self.code.upper()} to {i[0].upper()} exchange rate is {i[1]}")
+                self.code_in = input("Please, enter the currency to be transferred: ")
+                self.code_out = input("What currency would you like to transfer to?: ")
+        print(f"{self.coin * self.rate.request()} {self.code_out.upper()}")
+
+    def balance(self):
+        print("=" * 70)
+        print(f"Balance: {self.coin}")
+
+    def main(self):
+        while True:
+            if self.code_in == "".strip() or self.code_out == "".strip():
+                break
+            else:
+                self.conversion()
+                self.code_out = value_error("What currency would you like to transfer to?: ", 1)
 
 
-def value_error(string):
+def value_error(string, param):
     while True:
-        try:
-            i = float(input(string))
-            return i
-        except ValueError:
-            print("Please enter a number")
+        if param == 1:
+            i = input(string).lower()
+            if i in code_current:
+                return i
+            else:
+                print("Please, enter the correct currency")
+        if param == 2:
+            try:
+                i = float(input(string))
+                return i
+            except ValueError:
+                print("Please, enter a number")
 
 
 if __name__ == "__main__":
-    code = input("Please, enter currency code: ")
-    a = CurrencyExchange(code)
-    a.conversion()
+    code_in = value_error("Please, enter the currency to be transferred: ", 1)
+    code_out = value_error("What currency would you like to transfer to?: ", 1)
+    my_coin = value_error("Please, enter the number of coins: ", 2)
+    a = CurrencyExchange(code_in, my_coin, code_out)
+    a.main()
--- Currency_Exchange/parse.py
+++ Currency_Exchange/parse.py
@@ -2,15 +2,45 @@ import requests
 
 
 class Parse:
-    def __init__(self, currency):
-        self.url = f"http://www.floatrates.com/daily/{currency}.json"
+    def __init__(self, currency_in, currency_out):
+        self.currency_in = currency_in
+        self.url = f"http://www.floatrates.com/daily/{self.currency_in}.json"
+        self.currency_out = currency_out
+        self.check = 0
         self.response = requests.get(self.url).json()
+        self.cache = {}
+        if self.currency_in == "usd":
+            self.cache = {"usd": 1, "eur": self.response["eur"]["rate"]}
+        elif self.currency_in == "eur ":
+            self.cache = {"eur": 1, "usd": self.response["usd"]["rate"]}
+        else:
+            self.cache = {"eur": self.response["eur"]["rate"], "usd": self.response["usd"]["rate"]}
 
     def request(self):
-        try:
-            return ['eur', self.response['eur']['rate']], ['usd', self.response['usd']['rate']]
-        except KeyError:
-            try:
-                return ['eur', 1], ['usd', self.response['usd']['rate']]
-            except KeyError:
-                return ['eur', self.response['eur']['rate']], ['usd', 1]
+        print("Checking the cache...")
+        if self.currency_in.lower() == self.currency_out:
+            print("It is in the cache!")
+            self.check = 0
+            return 1
+        elif self.currency_out in self.cache.keys():
+            print("It is in the cache!")
+            self.check = 0
+            return self.cache[self.currency_out]
+        else:
+            print("Sorry, but it is not in the cache!")
+            result = self.response[self.currency_out]["rate"]
+            self.cache.update({self.currency_out.lower(): result})
+            return result
+
+
+code_current = ['eur', 'gbp', 'cad', 'jpy', 'aud', 'chf', 'lrd', 'sdg', 'top', 'vuv', 'hkd', 'thb', 'xaf', 'mdl', 'uyu',
+                'mga', 'ttd', 'lak', 'bwp', 'jod', 'bgn', 'vnd', 'uzs', 'nio', 'cve', 'aoa', 'khr', 'nok', 'lkr', 'pln',
+                'pen', 'iqd', 'stn', 'xpf', 'hnl', 'scr', 'dop', 'nzd', 'hrk', 'dzd', 'ars', 'bnd', 'kmf', 'lsl', 'tzs',
+                'bbd', 'ang', 'pkr', 'krw', 'azn', 'crc', 'jmd', 'ssp', 'mru', 'mnt', 'brl', 'egp', 'sgd', 'zar', 'kgs',
+                'pyg', 'srd', 'ghs', 'cup', 'dkk', 'inr', 'try', 'twd', 'tmt', 'afn', 'svc', 'sbd', 'zmw', 'yer', 'lbp',
+                'huf', 'ngn', 'irr', 'mkd', 'bif', 'all', 'mur', 'ves', 'npr', 'isk', 'gip', 'gel', 'bzd', 'gnf', 'szl',
+                'sos', 'aed', 'php', 'ils', 'mro', 'cop', 'gyd', 'rwf', 'ern', 'wst', 'cny', 'sar', 'myr', 'kzt', 'pab',
+                'nad', 'syp', 'mop', 'bam', 'idr', 'tnd', 'xof', 'tjs', 'bob', 'etb', 'xcd', 'mwk', 'gtq', 'kwd', 'czk',
+                'pgk', 'uah', 'gmd', 'awg', 'mmk', 'mvr', 'sek', 'mad', 'ron', 'byn', 'rsd', 'bsd', 'djf', 'sll', 'kes',
+                'bhd', 'omr', 'rub', 'lyd', 'clp', 'fjd', 'cdf', 'mzn', 'ugx', 'bdt', 'qar', 'mxn', 'amd', 'htg',
+                'usd', '']
