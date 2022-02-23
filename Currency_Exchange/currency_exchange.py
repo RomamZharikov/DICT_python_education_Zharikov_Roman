b9faeb64befd29a44f3a02053300d4e69837c847
Roman Zharikov <r.o.zharikov@student.khai.edu>
2022-02-23 23:18:34 +0200

    Currency Exchange 5-th stage (New Functions)

--- Currency_Exchange/currency_exchange.py
+++ Currency_Exchange/currency_exchange.py
@@ -1,38 +1,64 @@
 from parse import Parse, code_current
-from json import JSONDecodeError
 
 
 class CurrencyExchange:
-    def __init__(self, current_code_in, coin, current_code_out):
+    def __init__(self, current_code_in, coin):
         self.code_in = current_code_in
-        self.code_out = current_code_out
+        self.code_out = ""
         self.coin = coin
+        self.coin_remainder = coin
         self.used_coin = 0
+        self.use = int
         self.rate = Parse(self.code_in, self.code_out.lower())
+        self.command = ["!balance", "!transactions", "!exchange", "!leave"]
+        self.conversions = []
 
     def conversion(self):
-        while True:
-            try:
-                self.rate.currency_in = self.code_in
-                self.rate.currency_out = self.code_out.lower()
-                break
-            except JSONDecodeError:
-                print("Please, enter a valid currency code")
-                self.code_in = input("Please, enter the currency to be transferred: ")
-                self.code_out = input("What currency would you like to transfer to?: ")
-        print(f"{self.coin * self.rate.request()} {self.code_out.upper()}")
+        self.rate.currency_in = self.code_in
+        self.rate.currency_out = self.code_out.lower()
+        self.conversions.append(f"{self.use} {self.code_in.upper()} --> " +
+                                f"{self.use * self.rate.request()} {self.code_out.upper()}")
+        self.coin_remainder -= self.use
+        print(round(float(self.conversions[-1].split()[-2]), 2), self.conversions[-1].split()[-1].upper())
 
     def balance(self):
         print("=" * 70)
-        print(f"Balance: {self.coin}")
+        print(f"""Balance: 
+        * At the beginning it was: {self.coin} {self.code_in.upper()}
+        * Remainder: {self.coin_remainder} {self.code_in.upper()}""")
 
-    def main(self):
+    def menu(self):
         while True:
-            if self.code_in == "".strip() or self.code_out == "".strip():
+            choice = input("=" * 70 + "\nPlease enter the command: ").lower().strip()
+            if choice == "!balance" or choice == "!b":
+                self.balance()
+            elif choice == "!help" or choice == "!h":
+                for i in self.command:
+                    print(f"* {i}")
+            elif choice == "!transactions" or choice == "!t":
+                for i in range(len(self.conversions)):
+                    print(f"{i + 1}). {self.conversions[i]}")
+            elif choice == "!exchange" or choice == "!e":
+                self.exchange()
+            elif choice == "!leave" or choice == "!l":
                 break
             else:
-                self.conversion()
-                self.code_out = value_error("What currency would you like to transfer to?: ", 1)
+                print("Please, imput correct command!")
+
+    def exchange(self):
+        print("=" * 70)
+        self.code_out = value_error("What currency would you like to transfer to?: ", 1)
+        while True:
+            self.use = value_error("How much do you want to use?: ", 2)
+            if self.use <= self.coin_remainder:
+                break
+            else:
+                print(f"Insufficient funds. Left {self.coin_remainder} {self.code_in.upper()}")
+        if self.code_in == "".strip() or self.code_out == "".strip():
+            print("You have entered the menu!")
+            return
+        else:
+            self.conversion()
 
 
 def value_error(string, param):
@@ -42,18 +68,18 @@ def value_error(string, param):
             if i in code_current:
                 return i
             else:
-                print("Please, enter the correct currency")
+                print("Please, enter the correct currency!\n" + "=" * 70)
         if param == 2:
             try:
                 i = float(input(string))
                 return i
             except ValueError:
-                print("Please, enter a number")
+                print("Please, enter a number!\n" + "=" * 70)
 
 
 if __name__ == "__main__":
-    code_in = value_error("Please, enter the currency to be transferred: ", 1)
-    code_out = value_error("What currency would you like to transfer to?: ", 1)
+    print("=" * 70 + "\nWelcome to currency converter!\n" + "=" * 70)
+    code_in = value_error("Please, enter your main currency: ", 1)
     my_coin = value_error("Please, enter the number of coins: ", 2)
-    a = CurrencyExchange(code_in, my_coin, code_out)
-    a.main()
+    a = CurrencyExchange(code_in, my_coin)
+    a.menu()
